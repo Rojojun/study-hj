9286576d6e2fe925d3271c7719beff010e25c6dd
rojojun <zoloman316@gmail.com>
2025-12-06 11:57:00 +0900

    gRPC 학습 기록

--- causal_consistency.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from pymongo.read_concern import ReadConcern
-from pymongo.write_concern import WriteConcern
-
-conn = "mongodb://localhost:27017, localhost:27018, localhost:27019"
-
-client = MongoClient(conn)
-with client.start_session(causal_consistency = True) as session:
-    db = client.test
